d6329d57b97fa6bf6ddb7d73e8e43bfcd5481526
Justin Mimbs <justin.mimbs@gmail.com>
2019-06-01 15:18:18 -0400

    Update main to accept arg

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,28 @@
 mod dictionary;
 
+use std::env;
+use std::process;
 use wordsearch::{Board, Dict};
 
 fn main() {
+    let args = env::args();
+
+    let board_str = args.skip(1).next().unwrap_or_else(|| {
+        eprintln!("Argument error: missing argument");
+        process::exit(1);
+    });
+
+    let board: Board = match board_str.parse() {
+        Ok(board) => board,
+        Err(message) => {
+            eprintln!("Argument error: {}", message);
+            process::exit(1);
+        }
+    };
+
     let dict: Dict = dictionary::WORDS.iter().map(|s| *s).collect();
+    let paths = board.search(&dict);
+    let words: Vec<String> = paths.iter().map(|path| board.path_to_word(path)).collect();
 
-    if let Ok(board) = "blu mar ten".parse::<Board>() {
-        let paths = board.search(&dict);
-        let words: Vec<String> = paths.iter().map(|path| board.path_to_word(path)).collect();
-
-        println!("{:?}", words);
-    }
+    println!("{:?}", words);
 }
